b2b271d83c98732f90adb21580903aca10b2d7c3
İlkay Çitak <ilkay.citak@arfitect.com>
2021-12-28 10:48:02 +0300

    Add project files.

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using Domain;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public int MyProperty { get; set; }
+        public DataContext([NotNullAttribute] DbContextOptions options) : base(options) 
+        {
+
+        }
+        public DbSet<Activity> Activities  { get; set; }
+    }
+}
