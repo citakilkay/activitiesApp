0e8ba67b45596c7c4248d70b32573d72efa2703b
İlkay Çitak <ilkay.citak@arfitect.com>
2022-01-01 19:13:36 +0300

    Create CRUD operations

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,4 +1,6 @@
-﻿using Domain;
+﻿using Application.Activities;
+using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -11,20 +13,36 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
-        public ActivitiesController(DataContext context)
+        private readonly IMediator _mediator;
+        public ActivitiesController(IMediator mediator)
         {
-            _context = context;
+            _mediator = mediator;
         }
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+            return await _mediator.Send(new List.Query { });
         }
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivityById(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
+            return await _mediator.Send(new Details.Query { Id = id });
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity) // IActionResult 
+        {
+            return Ok(await _mediator.Send(new Create.Command { Activity = activity }));
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await _mediator.Send(new Edit.Command { Activity = activity }));
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await _mediator.Send(new Delete.Command { Id = id }));
         }
     }
 }
---
+++ Application/Activities/Edit.cs
@@ -0,0 +1,39 @@
+﻿using Domain;
+using MediatR;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Activities
+{
+    public class Edit
+    {
+        public class Command : IRequest {
+            public Activity Activity { get; set; }
+        }
+        public class Handler : IRequestHandler<Command>
+        {
+            public DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Activity.Id);
+
+                activity.Title = request.Activity.Title ?? activity.Title;
+                activity.Category = request.Activity.Category ?? activity.Category;
+                activity.City = request.Activity.City ?? activity.City;
+                activity.Description = request.Activity.Description ?? activity.Description;
+                activity.Venue = request.Activity.Venue ?? activity.Venue;
+                return Unit.Value;
+
+            }
+        }
+    }
+}
