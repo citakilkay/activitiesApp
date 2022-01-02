3de1d3cdaad4cfd737c3b48614959991afb6f4a7
İlkay Çitak <ilkay.citak@arfitect.com>
2022-01-02 11:22:24 +0300

    automapper

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -1,4 +1,5 @@
-﻿using Domain;
+﻿using AutoMapper;
+using Domain;
 using MediatR;
 using Persistence;
 using System;
@@ -17,20 +18,18 @@ namespace Application.Activities
         }
         public class Handler : IRequestHandler<Command>
         {
-            public DataContext _context;
-            public Handler(DataContext context)
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper)
             {
                 _context = context;
+                _mapper = mapper;
             }
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities.FindAsync(request.Activity.Id);
 
-                activity.Title = request.Activity.Title ?? activity.Title;
-                activity.Category = request.Activity.Category ?? activity.Category;
-                activity.City = request.Activity.City ?? activity.City;
-                activity.Description = request.Activity.Description ?? activity.Description;
-                activity.Venue = request.Activity.Venue ?? activity.Venue;
+                _mapper.Map(request.Activity, activity);
                 return Unit.Value;
 
             }
